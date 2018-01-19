3ecc9f4e70e00706804686e482aead07a18d6872
Silvio Moreira <silvio.moreira@qntfy.com>
2018-01-18 20:12:04 -0500

    fix meta script

--- experiments/asma/scripts/run_sentiment.sh
+++ experiments/asma/scripts/run_sentiment.sh
@@ -2,14 +2,16 @@ set -e
 sentiment_datasets="boo-cheer cameron-3 clacton clegg debate-1 farage miliband-1 miliband-2"
 # sentiment_datasets="boo-cheer cameron-3"
 
+echo "" > "done_sentiment.txt"
 for ds in $sentiment_datasets; 
 do
     ./experiments/asma/scripts/supervised_ASMA.sh $ds sentiment.txt
-    echo $ds >> "done.txt"
+    echo $ds >> "done_sentiment.txt"
 done
 
+echo "" > "done_sentiment_bin.txt"
 for ds in $sentiment_datasets; 
 do
     ./experiments/asma/scripts/supervised_ASMA.sh $ds~BINARY sentiment~BINARY.txt
-    echo $ds >> "done.txt"
+    echo $ds >> "done_sentiment_bin.txt"
 done
--- experiments/asma/scripts/run_topics.sh
+++ experiments/asma/scripts/run_topics.sh
@@ -1,8 +1,9 @@
 set -e
-topic_datasets="duggan-1 duggan-2 duggan-3 cameron-2 cameron-1 flood-1"
+topic_datasets="duggan-1 duggan-3 cameron-2 cameron-1 flood-1"
 
+echo "" > "done_topics.txt"
 for ds in $topic_datasets; 
 do
     ./experiments/asma/scripts/supervised_ASMA.sh $ds topics.txt
-    echo $ds >> "done.txt"
+    echo $ds >> "done_topics.txt"
 done
